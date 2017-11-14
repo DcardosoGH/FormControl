30e0a721de06129303545a8dbbfc5a4f3d0614c9
Pedro Esperança <buesimples@gmail.com>
2017-11-14 21:18:34 +0000

    updating angular and including effects from VM

--- src/state/user/user.effects.ts
+++ src/state/user/user.effects.ts
@@ -1,23 +1,22 @@
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/do';
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import { Action } from '@ngrx/store';
-import { Actions, Effect } from '@ngrx/effects';
-import { of } from 'rxjs/observable/of';
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/mergeMap";
+import "rxjs/add/operator/catch";
+import "rxjs/add/operator/do";
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { Observable } from "rxjs/Observable";
+import { Action } from "@ngrx/store";
+import { Actions, Effect } from "@ngrx/effects";
+import { of } from "rxjs/observable/of";
 
-import * as UserActions from './user.actions';
-import * as Encript from './../../state/user/sha2';
-import { log } from 'util';
+import * as UserActions from "./user.actions";
+import * as Encript from "./../../state/user/sha2";
+import { log } from "util";
 export type Action = UserActions.All;
 
 @Injectable()
 export class UserEffects {
-
   LoginRequest: Function;
   EncodeLogin: Function;
 
@@ -27,46 +26,59 @@ export class UserEffects {
     private router: Router
   ) {
     this.LoginRequest = (username, password) => {
-      const encPass = Encript.b64_sha512(password);
+      const encPass = Encript.b64_sha512(password) + "==";
+
       return {
-        "ServerRequest":"LoginRequest",
-        "RequestData":{"LoginUser":username,
-        "LoginHashPassword":encPass,
-        "LoginPassword":null,
-        "LoginIIS":false},
-        "RequestSession":null
-      }
+        ServerRequest: "LoginRequest",
+        RequestData: {
+          LoginUser: username,
+          LoginHashPassword: encPass,
+          LoginPassword: null,
+          LoginIIS: false
+        },
+        RequestSession: null
+      };
     };
 
-    this.EncodeLogin = (req) => {
-      const encodedurl = "ajax_req=" + encodeURIComponent(req);
+    this.EncodeLogin = req => {
+      console.log(req, JSON.stringify(req));
+
+      const encodedurl = "ajax_req=" + encodeURIComponent(JSON.stringify(req));
+      console.log(encodedurl);
+
       return encodedurl;
     };
   }
 
-  @Effect() login$: Observable<Action> = this.actions$.ofType(UserActions.USER_LOG_IN)
+  @Effect()
+  login$: Observable<Action> = this.actions$
+    .ofType(UserActions.USER_LOG_IN)
     .mergeMap(action =>
       // [ Hack ] should typeckeck `payload`
-      this.http.post(
-        'https://jsonplaceholder.typicode.com/posts/1',
-        this.EncodeLogin(
-          this.LoginRequest(
-            action['payload'].username,
-            action['payload'].password
-          )
-        ),
-        { headers: new HttpHeaders().set('Authorization', 'my-auth-token'), }
-      )
-        .do(data => console.log('payload', action['payload']))
-        .do(data => this.LoginRequest(action['payload'].username, action['payload'].password))
-        .do(data => this.LoginRequest(action['payload'].username, action['payload'].password))
+      this.http
+        .post(
+          "http://192.168.50.100/PassWeb/AxonHandler.ashx",
+          this.EncodeLogin(
+            this.LoginRequest(
+              action["payload"].username,
+              action["payload"].password
+            )
+          ),
+          {
+            headers: new HttpHeaders()
+              .set("Content-Type", "application/x-www-form-urlencoded")
+              .set("responseType", "text")
+          }
+        )
+        .do(data => console.log("data", data))
         // .do(data => this.router.navigate(['home']))
-        .map(data => ({ type: UserActions.USER_LOG_IN_SUCCESS, payload: {
-          username: action['payload'].username,
-          password: action['payload'].password,
-        } }))
-
-        // .catch(() => of({ type: UserActions.ADD_TODO_FAIL }))
+        .map(data => ({
+          type: UserActions.USER_LOG_IN_SUCCESS,
+          payload: {
+            username: action["payload"].username,
+            password: action["payload"].password
+          }
+        }))
+        .catch(err => of({ type: UserActions.USER_LOG_IN_FAIL, payload: err }))
     );
-
 }
